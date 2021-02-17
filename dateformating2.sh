a83045b7f2016a07520cb2d97699cb05b33dc10f
sri <sri@test.com>
2021-02-17 11:10:35 +0000

    added

---
+++ dateformating2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+########
+#!/bin/bash
+now="$(date)"
+printf "Current date and time %s\n" "$now"
+now="$(date +'%d/%m/%Y')"
+printf "Current date in dd/mm/yyyy format %s\n" "$now"
+
+echo "Starting backup at $now, please wait... till the end"
+#######
